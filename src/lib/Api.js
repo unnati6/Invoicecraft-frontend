f358844cdf093814a5f178680401261c7bbfcd5d
Unnati <unnatiparmarcp@gmail.com>
2025-06-12 11:08:04 +0530

    Added Render link 2

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -1,1 +1,3 @@
-export const BASE_URL = 'https://invoicecraft-backend.onrender.com';
+//export const BASE_URL = 'http://localhost:5000/api';
+
+export const BASE_URL = 'https://invoicecraft-backend.onrender.com/api';
