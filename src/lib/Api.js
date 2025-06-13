f570c06b71c84ada74c68c9c751592bca9641ba9
Unnati <unnatiparmarcp@gmail.com>
2025-06-13 18:51:48 +0530

    Added Msa template and cover page 1

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -1,3 +1,3 @@
- export const BASE_URL = 'http://localhost:5000/api';
+ //export const BASE_URL = 'http://localhost:5000/api';
 export const API_KEY = 'ac21ff5393msh60cf95caf40ca8dp189dd9jsn3d192b9a15d6'
-//export const BASE_URL = 'https://invoicecraft-backend.onrender.com/api';
+export const BASE_URL = 'https://invoicecraft-backend.onrender.com/api';
