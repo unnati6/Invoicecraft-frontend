a62a97bdff836d1eab58a37b475aa0b417e13a6a
Unnati <unnatiparmarcp@gmail.com>
2025-06-20 18:53:59 +0530

    purchase order don 2

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000/api';
-//export const BASE_URL = 'https://invoicecraft-backend.onrender.com/api';
+//export const BASE_URL = 'http://localhost:5000/api';
+export const BASE_URL = 'https://invoicecraft-backend.onrender.com/api';
